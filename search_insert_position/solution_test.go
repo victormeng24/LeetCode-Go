3ad9d5386b4bcc667cb4ab679861465767b00b8b
rogermeng <rogermeng@tencent.com>
2020-07-22 15:25:47 +0800

    20200722：count_and_say search_insert_position

---
+++ search_insert_position/solution_test.go
@@ -0,0 +1,56 @@
+package search_insert_position
+
+import "testing"
+
+func Test_searchInsert(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 5,
+			},
+			want: 2,
+		},
+		{
+			name: "case2",
+			args: args{
+				nums:   []int{1, 3, 5, 2},
+				target: 2,
+			},
+			want: 1,
+		},
+		{
+			name: "case3",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 7,
+			},
+			want: 4,
+		},
+		{
+			name: "case4",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchInsert(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInsert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
