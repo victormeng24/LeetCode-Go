0216f76a310ea58dc1684883289f7d7e35dfb690
rogermeng <rogermeng@tencent.com>
2020-08-06 14:47:48 +0800

    202008006 reverse_linked_list running_sum_of_1d_array

---
+++ reverse_linked_list/solution_test.go
@@ -0,0 +1,34 @@
+package reverse_linked_list
+
+import (
+	. "LeetCode-Go/common"
+	"reflect"
+	"testing"
+)
+
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				BuildList([]int{1, 2, 3, 4}),
+			},
+			want: BuildList([]int{4, 3, 2, 1}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
