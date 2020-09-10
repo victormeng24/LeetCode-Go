c5e1851751206b150cb8c149a3a008a2ff1a159d
rogermeng <rogermeng@tencent.com>
2020-09-10 15:50:26 +0800

    20200910 longest_palindrome minimum_depth_of_binary_tree

---
+++ longest_palindrome/solution_test.go
@@ -0,0 +1,30 @@
+package longest_palindrome
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				s: "abccccdd",
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
