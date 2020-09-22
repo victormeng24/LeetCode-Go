67c3cc18b993419332cf3a3faeefb9d71cab0757
rogermeng <rogermeng@tencent.com>
2020-09-22 11:17:59 +0800

    20200922 coin_change combination_sum

---
+++ coin_change/solution_test.go
@@ -0,0 +1,40 @@
+package coin_change
+
+import "testing"
+
+func Test_coinChange(t *testing.T) {
+	type args struct {
+		coins  []int
+		amount int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				coins: []int{1, 2, 5},
+				amount: 11,
+			},
+			want: 3,
+		},
+		{
+			name: "case2",
+			args: args{
+				coins: []int{2},
+				amount: 3,
+			},
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := coinChange(tt.args.coins, tt.args.amount); got != tt.want {
+				t.Errorf("coinChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
