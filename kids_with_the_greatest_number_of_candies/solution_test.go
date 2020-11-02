c8020d7052eb3d08fa11ef52c34e0188758562e4
rogermeng <rogermeng@tencent.com>
2020-11-02 15:56:30 +0800

    20201102 kids_with_the_greatest_number_of_candies

---
+++ kids_with_the_greatest_number_of_candies/solution_test.go
@@ -0,0 +1,43 @@
+package kids_with_the_greatest_number_of_candies
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_kidsWithCandies(t *testing.T) {
+	type args struct {
+		candies      []int
+		extraCandies int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				candies: []int{2, 3, 5, 1, 3},
+				extraCandies: 3,
+			},
+			want: []bool{true, true, true, false, true},
+		},
+		{
+			name: "case2",
+			args: args{
+				candies: []int{4, 2, 1, 1, 2},
+				extraCandies: 1,
+			},
+			want: []bool{true, false, false, false, false},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := kidsWithCandies(tt.args.candies, tt.args.extraCandies); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("kidsWithCandies() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
