66131abd88b92e83e5da9e4a0d81bc1ce1ca5ae0
rogermeng <rogermeng@tencent.com>
2020-11-05 15:25:05 +0800

    20201105 valid_anagram

---
+++ valid_anagram/solution_test.go
@@ -0,0 +1,40 @@
+package valid_anagram
+
+import "testing"
+
+func Test_isAnagram(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				s: "anagram",
+				t: "nagaram",
+			},
+			want: true,
+		},
+		{
+			name: "case2",
+			args: args{
+				s: "rat",
+				t: "pig",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAnagram(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isAnagram() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
