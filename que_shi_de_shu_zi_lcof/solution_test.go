77317d356213529bb64b0d576d9162a7fe64c71a
rogermeng <rogermeng@tencent.com>
2020-11-11 19:50:39 +0800

    20201111 que_shi_de_shu_zi_lcof

---
+++ que_shi_de_shu_zi_lcof/solution_test.go
@@ -0,0 +1,30 @@
+package que_shi_de_shu_zi_lcof
+
+import "testing"
+
+func Test_missingNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				nums: []int{0, 1, 3},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := missingNumber(tt.args.nums); got != tt.want {
+				t.Errorf("missingNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
